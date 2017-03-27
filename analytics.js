2eb4ed1f1d60efde49047f2e7ab69515af92232f
Kyle Paul <kpaul86+github@gmail.com>
2017-03-27 05:24:01 -0500

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,11 +1,24 @@
 var analytics = new Firebase('https://fir-analytics-9bac7.firebaseio.com');
 var activeVisitors = analytics.child('activeVisitors');
 
-activeVisitors.push({
+var visitor = {
   path: window.location.pathname,
   arrivedAt: Firebase.ServerValue.TIMESTAMP,
   userAgent: navigator.userAgent
+};
+
+var activeVisitorRef = activeVisitors.push(visitor, function () {
+  activeVisitors.child(visitorId).once('value', function (snapshot) {
+    visitor.arrivedAt = snapshot.child('arrivedAt').val();
+    var pastVisitors = analytics.child('pastVisitors');
+    visitor.leftAt = Firebase.ServerValue.TIMESTAMP;
+    pastVisitors.child(visitorId).onDisconnect().set(visitor);
+  });
 });
+
+var visitorId = activeVisitorRef.name();
+
+activeVisitorRef.onDisconnect().remove();
 
 var totalVisitors = analytics.child('totalVisitors');
 totalVisitors.transaction(function (currentData) {
