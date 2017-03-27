6e109d38f7c9dfefd42ee4c3564a886a841c7736
Kyle Paul <kpaul86+github@gmail.com>
2017-03-27 14:28:04 -0500

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,24 +1,24 @@
 (function (firebase) {
-  var analytics = new Firebase('https://fir-analytics-9bac7.firebaseio.com/');
+  var analytics = firebase.database().ref();
   var activeVisitors = analytics.child('activeVisitors');
 
   var visitor = {
     path: window.location.pathname,
-    arrivedAt: Firebase.ServerValue.TIMESTAMP,
+    arrivedAt: firebase.database.ServerValue.TIMESTAMP,
     userAgent: navigator.userAgent
   };
 
-  var activeVisitorRef = activeVisitors.push(visitor, function () {
-    activeVisitors.child(visitorId).once('value', function (snapshot) {
-      visitor.arrivedAt = snapshot.val().arrivedAt;
-      var pastVisitors = analytics.child('pastVisitors');
-      visitor.leftAt = Firebase.ServerValue.TIMESTAMP;
-      pastVisitors.child(visitorId).onDisconnect().set(visitor);
-      activeVisitorRef.onDisconnect().remove();
-    });
+  var activeVisitorRef = activeVisitors.push();
+  var visitorId = activeVisitorRef.key;
+  activeVisitorRef.set(visitor);
+  
+  activeVisitors.child(visitorId).once('value', function (snapshot) {
+    visitor.arrivedAt = snapshot.val().arrivedAt;
+    var pastVisitors = analytics.child('pastVisitors');
+    visitor.leftAt = firebase.database.ServerValue.TIMESTAMP;
+    pastVisitors.child(visitorId).onDisconnect().set(visitor);
+    activeVisitorRef.onDisconnect().remove();
   });
-
-  var visitorId = activeVisitorRef.name();
 
   var totalVisitors = analytics.child('totalVisitors');
   totalVisitors.once('value', function (snapshot) {
