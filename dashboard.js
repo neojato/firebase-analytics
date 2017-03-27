e8e039d672f3408f232ba28a5c0d9ccd4a0d297e
Kyle Paul <kpaul86+github@gmail.com>
2017-03-27 05:41:41 -0500

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,49 @@
+var analytics = new Firebase('https://fir-analytics-9bac7.firebaseio.com');
+
+$(document).on('ready', function () {
+  var $totalVisitors = $('#total-visitors');
+  analytics.child('totalVisitors').on('value', function (snapshot) {
+    $totalVisitors.text(snapshot.val());
+  });
+});
+
+var $activeVisitors = $('#active-visitors');
+var activeVisitors = analytics.child('activeVisitors');
+activeVisitors.on('child_added', function (snapshot) {
+  var n = snapshot.name();
+  var v = snapshot.val();
+  $activeVisitors.prepend(
+  '<li id="active-visitor' + n + '">' + n + ':' +
+    '<ul>' +
+      '<li>Arrived: ' + new Date(v.arrivedAt) + '</li>' +
+      '<li>Path: ' + v.path + '</li>' +
+      '<li>User Agent: ' + v.userAgent + '</li>' +
+    '</ul>' + 
+  '</li>'
+  );
+});
+
+activeVisitors.on('child_removed', function (snapshot) {
+  $('#active-visitor' + snapshot.name()).remove(); 
+});
+
+var pastVisitors = analytics.child('pastVisitors').endAt().limit(3);
+pastVisitors.on('child_added', function (snapshot) {
+  var n = snapshot.name();
+  var v = snapshot.val();
+  $pastVisitors.prepend(
+  '<li id="past-visitor' + n + '">' + n + ':' +
+    '<ul>' +
+      '<li>Arrived: ' + new Date(v.arrivedAt) + '</li>' +
+      '<li>Left: ' + new Date(v.leftAt) + '</li>' +
+      '<li>Spent: ' + ((v.leftAt - v.arrivedAt) / 1000) + ' Seconds </li>' +
+      '<li>Path: ' + v.path + '</li>' +
+      '<li>User Agent: ' + v.userAgent + '</li>' +
+    '</ul>' + 
+  '</li>'
+  );
+});
+
+pastVisitors.on('child_removed', function (snapshot) {
+  $('#past-visitor' + snapshot.name()).remove(); 
+});
