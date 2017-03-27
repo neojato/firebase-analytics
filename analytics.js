a849010653149fdeb48b443bd6b715ba43682564
Kyle Paul <kpaul86+github@gmail.com>
2017-03-27 13:12:03 -0500

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,34 +1,28 @@
-var analytics = new Firebase('https://fir-analytics-9bac7.firebaseio.com');
+(function (Firebase) {
+  var analytics = new Firebase('https://fir-analytics-9bac7.firebaseio.com/');
+  var activeVisitors = analytics.child('activeVisitors');
 
-firebase.auth().signInAnonymously().catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+  var visitor = {
+    path: window.location.pathname,
+    arrivedAt: Firebase.ServerValue.TIMESTAMP,
+    userAgent: navigator.userAgent
+  };
 
-var activeVisitors = analytics.child('activeVisitors');
-
-var visitor = {
-  path: window.location.pathname,
-  arrivedAt: Firebase.ServerValue.TIMESTAMP,
-  userAgent: navigator.userAgent
-};
-
-var activeVisitorRef = activeVisitors.push(visitor, function () {
-  activeVisitors.child(visitorId).once('value', function (snapshot) {
-    visitor.arrivedAt = snapshot.child('arrivedAt').val();
-    var pastVisitors = analytics.child('pastVisitors');
-    visitor.leftAt = Firebase.ServerValue.TIMESTAMP;
-    pastVisitors.child(visitorId).onDisconnect().set(visitor);
+  var activeVisitorRef = activeVisitors.push(visitor, function () {
+    activeVisitors.child(visitorId).once('value', function (snapshot) {
+      visitor.arrivedAt = snapshot.val().arrivedAt;
+      var pastVisitors = analytics.child('pastVisitors');
+      visitor.leftAt = Firebase.ServerValue.TIMESTAMP;
+      pastVisitors.child(visitorId).onDisconnect().set(visitor);
+      activeVisitorRef.onDisconnect().remove();
+    });
   });
-});
 
-var visitorId = activeVisitorRef.name();
+  var visitorId = activeVisitorRef.name();
 
-activeVisitorRef.onDisconnect().remove();
+  var totalVisitors = analytics.child('totalVisitors');
+  totalVisitors.once('value', function (snapshot) {
+    totalVisitors.set(snapshot.val() + 1);
+  });
 
-var totalVisitors = analytics.child('totalVisitors');
-totalVisitors.transaction(function (currentData) {
-  return currentData + 1;
-});
+})(Firebase);
